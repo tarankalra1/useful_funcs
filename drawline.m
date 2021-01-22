74115067eabcd9b1d6e443d358525db297d1ff59
Tarandeep Singh Kalra <tarankalra@gmail.com>
2021-01-22 16:07:43 -0500

    Create drawline.m

---
+++ drawline.m
@@ -0,0 +1,11 @@
+function [] = drawLine(p1, p2)
+theta = atan2( p2(2) - p1(2), p2(1) - p1(1));
+r = sqrt( (p2(1) - p1(1))^2 + (p2(2) - p1(2))^2);
+
+
+line = 0:0.000001: r;
+x = p1(1) + line*cos(theta);
+y = p1(2) + line*sin(theta);
+
+
+plot(x, y)
